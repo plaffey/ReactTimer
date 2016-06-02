7b2dbc4bbc857e20987ae1cb19719eaeab441316
plaffey <peter.laffey@gmail.com>
2016-06-01 23:52:55 -0500

    Initial commit

---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var Timer = React.createClass({
+  render: function () {
+    return (
+      <h1>Hello from the Timer component!</h1>
+    );
+  }
+});
+
+module.exports = Timer;
